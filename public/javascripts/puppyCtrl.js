2318a17b1fa703fb3269849abce7438ff84e5cb5
Daniel Lewis <dewis@sympletech.com>
2013-11-06 20:43:40 -0800

    angular2

--- public/javascripts/puppyCtrl.js
+++ public/javascripts/puppyCtrl.js
@@ -2,28 +2,45 @@ function puppyCtrl($scope, $http){
     $scope.puppies = [];
 
     $scope.loadPuppies = function(){
-        $scope.puppies.push({name : 'Koda', breed : 'Siberian Husky', age : '4'});
-        $scope.puppies.push({name : 'Yoda', breed : 'German Shepard', age : '6'});
-        $scope.puppies.push({name : 'Ginger', breed : 'Pound Puppy', age : '10'});
-        $scope.puppies.push({name : 'Dora', breed : 'Dingo', age : '5'});
+        $scope.puppies.push({_id:1, name : 'Koda', breed : 'Siberian Husky', age : '4'});
+        $scope.puppies.push({_id:2, name : 'Yoda', breed : 'German Shepard', age : '6'});
+        $scope.puppies.push({_id:3, name : 'Ginger', breed : 'Pound Puppy', age : '10'});
+        $scope.puppies.push({_id:4, name : 'Dora', breed : 'Dingo', age : '5'});
     };
     $scope.loadPuppies();
 
-    $scope.puppyForm = {
-        name : '',
-        breed : '',
-        age : ''
+    $scope.puppyForm = {};;
+    var resetForm = function(){
+        $scope.puppyForm = {
+            _id : '',
+            name : '',
+            breed : '',
+            age : ''
+        };
     };
+    resetForm();
 
     $scope.editPuppy = function(puppy){
-        //display Puppy to edit
+        $scope.puppyForm = angular.copy(puppy);
     };
 
     $scope.deletePuppy = function(puppy){
-        //Remove Puppy
+        $scope.puppies = _.reject($scope.puppies, function(p){
+            return p._id === puppy._id;
+        });
     };
 
     $scope.submitForm = function(){
-        //Add or Update Puppy
+        var existing = _.find($scope.puppies, function(p){
+            return p._id == $scope.puppyForm._id;
+        });
+
+        if(existing != null){
+            var i = _.indexOf($scope.puppies, existing);
+            $scope.puppies[i] = angular.copy($scope.puppyForm);
+        }else{
+            $scope.puppies.push(angular.copy($scope.puppyForm));
+        }
+        resetForm();
     };
 }
