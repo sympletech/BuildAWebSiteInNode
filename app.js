0c7b7435f5984b81da2ca349523686149ccd1183
Daniel Lewis <dewis@sympletech.com>
2013-11-06 20:28:10 -0800

    routing

--- app.js
+++ app.js
@@ -4,11 +4,9 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var routes = require('./routes');
 var app = express();
 
 // all environments
@@ -29,8 +27,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes.RegisterAppRoutes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
